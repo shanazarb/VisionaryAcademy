059b2bf3a301dcc194d6e1a484a29433882d79a2
Shanazar <shabegenjov@gmail.com>
2025-03-12 23:16:40 +0500

    Complete search

---
+++ js/universities.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchInput");
+  const universityContainers = document.querySelectorAll(".content");
+
+  searchInput.addEventListener("input", function () {
+    const searchText = searchInput.value.toLowerCase();
+
+    universityContainers.forEach((container) => {
+      const universityName = container
+        .querySelector(".content-name_heading")
+        .textContent.toLowerCase();
+      const universityLocation = container
+        .querySelector(".content-name_heading_s")
+        .textContent.toLowerCase();
+
+      if (
+        universityName.includes(searchText) ||
+        universityLocation.includes(searchText)
+      ) {
+        container.style.display = "block";
+      } else {
+        container.style.display = "none";
+      }
+    });
+  });
+});
